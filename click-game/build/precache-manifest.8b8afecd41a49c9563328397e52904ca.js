e0552e7a3de06317a8f11a5f19a5d43e2372cf3b
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-02-29 20:57:40 -0800

    functional clickygame

---
+++ click-game/build/precache-manifest.8b8afecd41a49c9563328397e52904ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c565f59cf4958af652ccaed47e7047",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "a84f0ffc4ba41d94aeb6",
+    "url": "/ClickyGame/static/css/main.90baf160.chunk.css"
+  },
+  {
+    "revision": "e2943051cc74af6711f2",
+    "url": "/ClickyGame/static/js/2.03d157f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ClickyGame/static/js/2.03d157f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84f0ffc4ba41d94aeb6",
+    "url": "/ClickyGame/static/js/main.24458103.chunk.js"
+  },
+  {
+    "revision": "2319a6dc19cf22630f0d",
+    "url": "/ClickyGame/static/js/runtime-main.f8458d13.js"
+  }
+]);
